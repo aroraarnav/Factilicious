3e083755162dadf5c1e2dba8975554eae0c2aa3a
Arnav Arora <aroraarnav2005@gmail.com>
2020-05-05 18:25:31 +0530

    Added Sign Out Functionality

---
+++ Factilicious/Factilicious/Main Scene/Tabs/Settings/SignOutViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Factilicious
+//
+//  Created by Arnav Arora on 05/05/20.
+//  Copyright © 2020 Jayant Arora. All rights reserved.
+//
+
+import UIKit
+
+class SignOutViewController: UIViewController {
+    
+    var defaults = UserDefaults.standard
+    
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        spinner.startAnimating()
+        defaults.set(false, forKey: "isSignedIn")
+        
+        let delay = 3 // seconds
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(delay)) {
+            self.spinner.stopAnimating()
+            self.performSegue(withIdentifier: "signOutSegue", sender: nil)
+        }
+    }
+    
+}
